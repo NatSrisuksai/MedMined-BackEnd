5fc90acaa76e854d8b782f007d1f08f002cbc166
NatSrisuksai <natsrisuksai02@gmail.com>
2025-09-14 02:09:05 +0700

    feat: allow all cors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // คุณมี rawBody อยู่แล้วสำหรับ LINE webhook
   const app = await NestFactory.create(AppModule, { rawBody: true });
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`Server running on http://localhost:${port}`);
+  const allowed = [
+    'http://localhost:3000', // Next dev
+    'http://localhost:8888', // ถ้าใช้ Netlify dev
+    'https://med-mind-sepia.vercel.app/',
+    process.env.FRONTEND_ORIGIN, // e.g. https://your-frontend.vercel.app
+  ].filter(Boolean) as string[];
+
+  app.enableCors({ origin: true })
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
