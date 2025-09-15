94384c85570a047a84bcdc4c5174393859e31665
NatSrisuksai <natsrisuksai02@gmail.com>
2025-09-15 23:14:29 +0700

    fix: import PrismaModule to PrescriptionModule

--- src/prescriptions/prescriptions.module.ts
+++ src/prescriptions/prescriptions.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrescriptionsController } from './prescriptions.controller';
 import { LineModule } from 'src/line/line.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [LineModule],
+  imports: [LineModule, PrismaModule],
   controllers: [PrescriptionsController],
   providers: [],
 })
